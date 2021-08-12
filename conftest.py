2df2dcb50ddbd7b9c1f62d74b09c00d7e40758bc
Ihor Lazariev <i.lazarew2001@gmail.com>
2021-08-12 23:10:52 +0300

    12.08.2021

---
+++ conftest.py
@@ -0,0 +1,21 @@
+
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', default='en',
+                     help="Choose language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    print("\nstarting chrome browser for test...")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    # options.headless = True
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquitting browser...")
+    browser.quit()
